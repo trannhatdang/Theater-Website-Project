eeb00c2963f1cf6556e617a6d1b987cc201a0eb8
pona <hihihibly@gmail.com>
2025-11-20 17:45:40 +0700

    oiaijsjdg

--- src/routers/root.router.js
+++ src/routers/root.router.js
@@ -1,5 +1,8 @@
 import express from "express";
 import filmRouter from "./film.router.js"
+import employeeRouter from "./employee.router.js"
+import eventRouter from "./event.router.js"
+import promotionRouter from "./promotion.router.js"
 
 const rootRouter = express.Router();
 
