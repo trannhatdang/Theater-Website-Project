a1a2a5e6060f031ade0d75ced4c775d26417274a
pona <hihihibly@gmail.com>
2025-11-20 17:08:09 +0700

    hihi

---
+++ src/static/employee/employee.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var replacers = document.querySelectorAll('[data-replace]');
+	console.log(replacers);
+	for(var i=0; i<replacers.length; i++){
+		let replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
+		Object.keys(replaceClasses).forEach(function(key) {
+			replacers[i].classList.remove(key);
+			replacers[i].classList.add(replaceClasses[key]);
+		});
+	}
+});
