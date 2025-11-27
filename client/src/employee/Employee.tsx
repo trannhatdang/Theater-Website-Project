d1bb1d5b66770956ec9976229256addb84ac9df8
trannhatdang <hihihibly@gmail.com>
2025-11-27 15:15:45 +0700

    routing

---
+++ client/src/employee/Employee.tsx
@@ -0,0 +1,3 @@
+export default function Employee(){
+	return <h1>eyy employee</h1>
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import HomePage from './home/homepage.tsx'
+import Employee from './employee/employee.tsx'
+import Dashboard from './dashboard/dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<HomePage />}
+				<Route path='/employee' element={<HomePage />}
+				<Route path='/dashboard' element={<HomePage />}
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>,
 )
